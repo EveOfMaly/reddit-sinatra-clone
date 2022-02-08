9678f30bf3f689b499855382baaf240a121dafe5
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-08 17:30:20 -0500

    created edit, show views and transfered information from new/show. edit them respectivelly

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -31,28 +31,55 @@ post "/posts" do
         @post.slug = "/posts/#{@post.id}"
         @post.save 
 
-        redirect "/subreddits"
+        redirect "/posts/#{@post.id}"
     end
 end
 
 
 get "/posts/:id" do 
+    @post = post.find_by(id: params[:id])
+    erb :'posts/show'
     #add delete post 
 end
 
 get "/posts/:id/edit" do 
-
+    @post = post.find_by(id: params[:id])
+    erb :'posts/edit'
 end
-
 
 patch "/posts/:id/" do 
 
+    if params[:post][:title] == "" || params[:post][:content] == ""
+        redirect "/post/new"
+    else
+        #change :url to :link
+
+        @post = Post.new(title: params[:post][:title], link: params[:post][:link], content: params[:post][:content])
+        @user = current_user
+
+        params[:sub_ids].each do |sub|
+            @sub = Sub.find(sub.to_i)
+            @post.subs << @sub
+        end
+
+        #if current user is owner 
+        @post.user = @user
+    
+        #associate post with subreddit 
+        @post.slug = "/posts/#{@post.id}"
+        @post.save 
+
+        redirect "/posts/#{@post.id}"
+    end
+
 end
 
-delete "/posts/:id/" do 
 
+
+delete "/posts/:id" do 
+    @post = post.find_by(id: params[:id])
+    @post.destory
 end
-
 
 
 end
