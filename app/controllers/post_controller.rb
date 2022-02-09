99050bc2be530b2beda3d568a513fc1423b5d040
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-09 12:08:47 -0500

    Subs not associating with post. Reviewing

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,17 +1,17 @@
 class PostController < ApplicationController
 
-# #create
 get "/posts" do 
 
 end
 
 #new
 
+#new post
 get '/posts/new' do 
     erb :"posts/new"
 end
 
-
+#create post 
 post "/posts" do 
     if params[:post][:title] == "" || params[:post][:content] == ""
         redirect "/post/new"
@@ -35,51 +35,52 @@ post "/posts" do
     end
 end
 
-
+#get posts
 get "/posts/:id" do 
-    @post = post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
     erb :'posts/show'
-    #add delete post 
 end
 
+#edit posts
 get "/posts/:id/edit" do 
-    @post = post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
     erb :'posts/edit'
 end
 
-patch "/posts/:id/" do 
+#update
+patch '/posts/:id' do 
 
-    if params[:post][:title] == "" || params[:post][:content] == ""
+    #moderator is allowed to update the title and description
+    if params[:post][:title] == "" || params[:post][:content] == "" || params[:post][:link] == "" 
         redirect "/post/new"
     else
-        #change :url to :link
-
-        @post = Post.new(title: params[:post][:title], link: params[:post][:link], content: params[:post][:content])
-        @user = current_user
-
-        params[:sub_ids].each do |sub|
-            @sub = Sub.find(sub.to_i)
-            @post.subs << @sub
-        end
-
-        #if current user is owner 
-        @post.user = @user
-    
-        #associate post with subreddit 
-        @post.slug = "/posts/#{@post.id}"
-        @post.save 
-
-        redirect "/posts/#{@post.id}"
+        binding.pry
+        #if post.subs.owner == current_user    
+            #make changes
+            
+        # # if @post.user == current_user 
+        #     @post = Post.find_by(id: params[:id])
+        #     @post.update(title: params[:post][:title], link: params[:post][:link], content: params[:post][:content])
+        #     params[:sub_ids].each do |sub|
+        #         @sub = Sub.find(sub.to_i)
+        #         @post.subs << @sub
+        #     end
+        #     @post.slug = "/posts/#{@post.id}" 
+        #     @post.save 
+        #     redirect "/posts/#{@post.id}"
+        # # else
+        # #     "failure"
+        # # end
     end
-
 end
 
 
 
-delete "/posts/:id" do 
-    @post = post.find_by(id: params[:id])
-    @post.destory
-end
+    delete "/posts/:id" do 
+        @post = Post.find_by(id: params[:id])
+        @post.destroy
+        redirect "/subreddits"
+    end
 
 
 end
