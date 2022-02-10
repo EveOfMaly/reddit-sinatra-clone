fa57936203a09c8f5efa0b070d4b9f7bc693a375
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-10 14:51:02 -0500

    Updated User Create and Update routes so you can't create a username that already exist or update to one that already exists

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
     use Rack::Flash
     
 
-   #show
+   #show individual user profile
     get "/users/:slug" do 
         if logged_in?
             @user = User.find_by_slug(params[:slug])
@@ -13,7 +13,7 @@ class UserController < ApplicationController
         end
     end
 
-
+  #form to creatae an individual user profile
     #new
      get '/new' do 
         if !logged_in?
@@ -23,22 +23,24 @@ class UserController < ApplicationController
         end
     end
 
-    #create
+    #create an individual user profile
     post '/users' do 
     
             if params[:username] == "" || params[:password] == "" 
                 flash[:message] = "Unable to create a user. Please add a username and password."
                 redirect "/new"
-            end
-
-            @user = User.new(username: params[:username], password: params[:password])
-
-            if @user.save 
-                flash[:message] = "Account Successfully created." #message displayed
-                session[:user_id] = @user.id
-                erb :'users/homefeed'
-            else
-                redirect "/"
+            elsif User.find_by(username: params[:username])      
+                flash[:message] = "Unable to create a user. Please add a username and password."
+                redirect "/new"
+            else               
+                 @user = User.new(username: params[:username], password: params[:password])
+                 if @user.save 
+                    flash[:message] = "Account Successfully created." 
+                    session[:user_id] = @user.id
+                    erb :'users/homefeed'
+                else
+                    redirect "/"
+                end
             end
     end
 
@@ -55,14 +57,12 @@ class UserController < ApplicationController
     end
 
     post '/login' do 
-        #BobMassa
-        #cash
+
         user = User.find_by(username: params[:username])
        
         if user && user.authenticate(params[:password])
             flash[:message] = "Successfully logged in."
             session[:user_id] = user.id
-            binding.pry
             redirect  "/" 
         else 
             flash[:message] = "Login failed. Please try again."
@@ -74,6 +74,7 @@ class UserController < ApplicationController
     #edit user 
 
     get '/users/:slug/edit' do 
+  
         @user = User.find_by_slug(params[:slug])
     
         if logged_in? && @user.id == current_user.id
@@ -83,18 +84,31 @@ class UserController < ApplicationController
         end
     end
 
+
     patch '/users/:slug' do  
-        @user = User.find_by_slug(params[:slug])
     
+        @user = User.find_by_slug(params[:slug])
+        
+        @user_find = User.find_by(username: params[:username])
+      
         if logged_in? && @user.id == current_user.id
             if params[:username] == "" 
                 redirect "/users/:slug/edit"
-            else 
-                @user.update(username: params[:username], cakeday: params[:cakeday])
-                @user.save
-                flash[:message] = "Profile sucesfully edited." #message worked.ß
-                redirect "/users/#{@user.slug}"
             end
+
+            
+           
+            if @user_find != nil && @user_find.username == params[:username] 
+                flash[:message] = "Unable to create a user. Already being used." 
+                redirect "/"
+            end
+            
+            @user.update(username: params[:username], cakeday: params[:cakeday])
+            
+            @user.save
+            flash[:message] = "Profile sucesfully edited."
+            redirect "/"
+           
         else
             flash[:message] = "Profile was not edited." #message worked.ß
             redirect "/"
